e2630fe2c9303091a84c1856c9204fc081f1128e
Joshua Ferguson <jwfergus@asu.edu>
2012-09-20 04:22:30 -0700

    Created and began implementing initial files. security_functions.py is a library of functions related to security within the MEL system. Message En/Decryption, key checking will likely occur here.

---
+++ security_functions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+##################
+# Key Check/Validation - Node side
+# v0.1
+# author: Joshua Ferguson <jwfergus@asu.edu>
+# 
+##################
+
+def key_check(key):
+	#
+	#	Gotta write this part!
+	#
+	return(key == 'secret key!')
