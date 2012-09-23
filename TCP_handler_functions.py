64312ea6faafdfa3a7976f2963ff1fc516f948d8
Joshua Ferguson <jwfergus@asu.edu>
2012-09-22 17:32:26 -0700

    Converted TCP_handler inside TCP_handler_functions.py to be a single function made to be called as it's own thread. It now loops over connections and within connections loops over RX'd transmissions adding them to the queue passed to it. Print statements have now been surrounded by if __debug__ flags which are removed when the python is run with the -O optimization flag.

--- TCP_handler_functions.py
+++ TCP_handler_functions.py
@@ -9,24 +9,33 @@
 
 import socket
 import Queue
+import sys
 
 def TCP_handler(queue, ip, port, buffer_size):
-
+	if __debug__:
+		print 'Inside TCP_handler thread'	
+	local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	local_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	local_socket.bind((ip,port))
+	local_socket.listen(1)	
 	while True:
-		local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		local_socket.bind((ip,port))
-		local_socket.listen(1)	
-		print 'TCP_handler thread waiting for connection . . .'
+		if __debug__:
+			print 'TCP_handler thread waiting for connection . . .'
 		remote_connection, remote_address = local_socket.accept()
-		print 'TCP_handler thread connection FOUND!'
+		if __debug__:
+			print 'TCP_handler thread connection FOUND!'
 		
 		while True:
-			command = remote_connection.recv()
+			command = remote_connection.recv(buffer_size)
 			if not command:
 				break
+			if __debug__:
+				if command == 'system exit':
+					print 'found system exit command'
+					sys.exit()
 			queue.put(command)
-			print 'TCP_handler command received: ', command
+			if __debug__:
+				print 'TCP_handler command received: ', command
 		remote_connection.close()
 		
 
-TCP_handler(Queue.Queue(0), '127.0.0.1', 4242, 1024)
