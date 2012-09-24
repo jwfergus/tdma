efe96e901b5d017d82703134de3f49c725d8ec01
joshua <jwfergus@asu.edu>
2012-09-24 10:51:11 -0700

    Complete re-write of the logic relying on TCP_handler thread for command queueing and blocking on that queue.

--- node.py
+++ node.py
@@ -11,6 +11,8 @@ import socket
 import security_functions
 from time import sleep
 from sys import exit
+import threading
+import Queue
 
 # Communication Variables
 ip = '127.0.0.1'
@@ -20,58 +22,45 @@ buffer_size = 1024
 # Logic Variables
 sleep_time = .990 #should be adjusted based on clock speed
 exit_message_received = False # Program exit command flag
-open_signal_received = False # Signal to open all connections
-
-local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-local_socket.bind((ip,port))
-local_socket.listen(1)
+open_connect = False # flag signifying open connections state
+command_queue = Queue.Queue(0)
 
 
-print 'Node - Connection address:', address
-
-# Check the key
+# Start TCP handler to begin receiving commands which are added to the command queue
+TCP_handler_thread = threading.Thread(target=TCP_handler_functions.TCP_handler, args=(command_queue, ip, port, buffer_size))
+TCP_handler_thread.start()
+time.sleep(0.5)
 
 
 
-
-
-while not exit_message_received:
+while True:
 	try:
-		open_signal_received = False
-		#OPEN CONNECTIONS
-		print 'Node - Opening all connections'
-		sleep(sleep_time)
-		
-		#CLOSE CONNECTIONS
-		print 'Node - Closing all connections'
-		while not open_signal_received:
-			# Check the key
-			data = connection.recv(buffer_size)
-			if not security_functions.key_check(data):
-				print 'inc security key. was: ', data
-				connection.close()
-				raise Exception('Node - incorrect security key - EXITING')
-			else:
-				print 'Node - client key validated' 
-			
-			while True:
-				while True:
-					data = connection.recv(buffer_size)
-					if data:
-						break
-					sleep(.005)
-				print 'Node - received data:', data
-				if data == 'open connections':
-					open_signal_received = True
-					break
-				if data == 'exit':
-					raise Exception('Node - Exit command received.')
+		# Close Connections
+		#
+		###################
+		open_connect = False
+		while not open_connect:
+			#block for queue command
+			encrypted_command = command_queue.get(True)
+			if security_functions.key_check(command):
+				if security_functions.get_command(encrypted_command) == 'open':
+					open_connect = True
+					# Open Connections
+					#
+					##################
+					time.sleep(sleep_time)
 					
-					
-	except Exception, exception_message:
-		print exception_message
-		connection.close()
+				elif security_functions.get_command(encrypted_command) == 'exit':
+					# Exit Cleanup
+					#
+					##############
+					exit()
+				else:
+					raise Exception('Command not recognized')
+	except Exception as exception:
+		print type(exception)
+		print exception.args
+		# Exit Cleanup
+		#
+		##############
 		exit()
-		
-
-
