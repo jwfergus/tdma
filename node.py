b6da1a7f561a2a32c2d3d11a4aed49c4ba9896c4
Joshua Ferguson <jwfergus@asu.edu>
2012-10-19 12:59:16 -0700

    added sleep time between sigusr1 and iptables command

--- node.py
+++ node.py
@@ -80,6 +80,7 @@ def execute(assigned_ip, assigned_open_connection_time):
 		
 def open_connections(pid):
 	os.system('iptables -D INPUT -p icmp -j DROP')
+	time.sleep(.1)
 	call(["kill", "-s", "SIGUSR1", str(pid)])
 
 def close_connections():
