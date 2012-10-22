26fbe2ba92c71890dcb9636d647a3e0558b82604
Joshua Ferguson <jwfergus@asu.edu>
2012-10-22 12:57:25 -0700

    Command center adjusted for demonstration on hardware

--- command_center.py
+++ command_center.py
@@ -21,29 +21,40 @@ import sys
 ##################
 def execute():
 	time.sleep(2.5)
-	ip = '192.168.1.101'
 	port = 4242
 	buffer_size = 1024
 	key = 'secretKey!'
 
-	number_list = (1,2,3,4,5,6,7)
-	for number in number_list:
-	
-		# Creates and opens socket to IP and issues secret key + open 
-		#	command
-		if __debug__:
-			print 'command_center.py - opening connection'
-		local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		local_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		local_socket.connect((ip,port))
-		local_socket.send(key +' open')
-		time.sleep(7)
-		local_socket.close()
-	
+	# application_list is, essentially, our system-user-application state
+	application_list = (('192.168.1.101', '192.168.1.102'),('192.168.1.103', '192.168.1.104'))
+	number_of_iterations = 50
 
-	if __debug__:
-		print 'command_center.py - opening connection for exit'
-	local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	local_socket.connect((ip,port))
-	local_socket.send(key +' exit')
+	for iteration in range(number_of_iterations):
+		for application_number in range(len(application_list)):
+			for box_to_start in application_list[application_number]:
+				# Creates and opens socket to IP and issues secret key + open 
+				#	command
+				if __debug__:
+					print 'command_center.py - opening connection to ' + box_to_start
+				local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				local_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				local_socket.connect((box_to_start,port))
+				local_socket.send(key +' open')
+				local_socket.close()
+			
+			time.sleep(5)
+	
+	for application_number in range(len(application_list)):
+		for box_to_start in application_list[application_number]:
+			# Creates and opens socket to IP and issues secret key + exit 
+			#	command
+			if __debug__:
+				print 'command_center.py - opening connection for exit to ' + box_to_start
+			local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			local_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			local_socket.connect((box_to_start,port))
+			local_socket.send(key +' exit')
+			local_socket.close()
+
+	
 	sys.exit()
