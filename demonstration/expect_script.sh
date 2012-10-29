de971f350715c0b84f25a338146933fb8ae100aa
Joshua Ferguson <jwfergus@asu.edu>
2012-10-29 12:02:37 -0700

    Created a script, takes arg= target_ip_addres to login as mel and update the MEL directory

---
+++ demonstration/expect_script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/expect -f
+
+##################
+# Expect Script
+# v0.1
+# author: Joshua Ferguson <jwfergus@asu.edu>
+# 
+# Script for testing updating demonstration repos
+##################
+
+
+
+# IMPORTANT VARIABLE, WILL NOT WORK WITHOUT CHANGE
+set pass "CBCT1305"
+
+# Get IP address as input
+set input_ip $argv
+set timeout 15
+
+# spawn the scp thread
+spawn ssh mel@$input_ip
+
+
+# When prompted for a password, give it (MUST BE SET AT THE TOP OF THE FILE)
+expect "password:" 
+send "$pass\r" 
+
+sleep 1
+send "sudo rm -r mutual-exclusion-logic/\r"
+sleep 1
+send "git clone git@impact.asu.edu:mutual-exclusion-logic.git\r"
+sleep 1
