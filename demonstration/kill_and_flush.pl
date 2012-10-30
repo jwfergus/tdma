43cc1822fd303ddf38877a89f94ba52327082ac8
Joshua Ferguson <jwfergus@asu.edu>
2012-10-30 13:17:30 -0700

    Added perl file to kill node side processes and flush iptables

---
+++ demonstration/kill_and_flush.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+##################
+# Stop all node-side code and flush iptables
+# v0.1
+# author: Joshua Ferguson <jwfergus@asu.edu>
+# 
+# Done in perl since python script would kill itself
+##################
+
+system("killall -9 python")
+system("killall packet_queuing_submodule")
+system("iptables --flush")
