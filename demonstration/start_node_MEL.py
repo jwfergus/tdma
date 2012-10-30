c3df2820073390f3ff9a91a4a4079b2c5159ce6b
Joshua Ferguson <jwfergus@asu.edu>
2012-10-30 15:27:01 -0700

    Made script and expect script to start MEL on nodes Updated the naming convention of the function inside of kill_and_flush.py

--- demonstration/start_node_MEL.py
+++ demonstration/start_node_MEL.py
@@ -10,11 +10,11 @@
 import os
 import sys
 
-def kill_and_flush():
+def start():
 	application_list = ('192.168.1.101', '192.168.1.102','192.168.1.103', '192.168.1.104')
 	for ip_address in application_list:
-		os.system("./kill_and_flush_expect.sh " + ip_address)
+		os.system("./start_node_MEL_expect.sh " + ip_address)
 
 
 if __name__ == "__main__":
-	kill_and_flush()
+	start()
---
+++ demonstration/start_node_MEL_expect.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/expect -f
+
+##################
+# Expect Script
+# v0.1
+# author: Joshua Ferguson <jwfergus@asu.edu>
+# 
+# Script for starting MEL on nodes
+##################
+
+
+
+# IMPORTANT VARIABLE, WILL NOT WORK WITHOUT CHANGE
+set pass "CBCT1305"
+
+# Get IP address as input
+set input_ip $argv
+set timeout 5
+
+# spawn the scp thread
+spawn ssh mel@$input_ip
+
+
+# When prompted for a password, give it (MUST BE SET AT THE TOP OF THE FILE)
+expect "password:" 
+send "$pass\r" 
+expect "$ "
+send "cd mutual-exclusion-logic\r" 
+expect "$ "
+send "sudo -k\r"
+expect "$ "
+send "make\r"
+expect "$ "
+send "sudo python node_main.py\r"
+expect "mel:"
+send "$pass\r" 
+expect "$ "
